a6ffe45f6ba1ea3dcc7f7b0808b9e86cc47207aa
root <root@kenyaemr-test1.(none)>
2013-11-26 13:25:25 -0800

    Fixed FTP access with resume and deleting existing VM

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,13 +3,13 @@
 #  oldVMName2Disable is in case this script is installed after the -
 #   VMs are setup, set this to the image to stop and delete i.e. "kenyaEMR APP"
 
-$oldVMName2Disable = "VM APP1"               # optional way to stop a VM.  skipped if blank
+$oldVMName2Disable = "KenyaVM_13.2.2-2"               # optional way to stop a VM.  skipped if blank
 $ovfImageName = "KenyaVM_13.2.2-2.ova"       # ovf file name, it's inside the torrent
-$virtualMachineName = ""                     # we will start the VM on success called [this]
+$virtualMachineName = "KenyaVM_13.2.2-2"                     # we will start the VM on success called [this]
 
 File["/opt/KenyaEMR"] ->
 Exec["download"] ->
-#Exec["disableoldVM"] ->
+Exec["disableoldVM"] ->
 #Exec["disableCurrentVM"] ->
 Exec["vboxImport"] ->
 Exec["startVM"]
@@ -18,27 +18,30 @@ file { "/opt/KenyaEMR" :
 	owner  => "puppet",
 	group  => "puppet",
 	ensure => "directory",
-	}
-exec { "download": 
-	command => "/usr/bin/wget ftp://54.201.64.239/KenyaVM_13.2.2-2.ova",
+     }
+
+exec { "download":
+	cwd => "/opt/KenyaEMR/", 
+	command => "/usr/bin/wget -c ftp://54.201.64.239/KenyaVM_13.2.2-2.ova",
 	timeout => 10800, #3 hours - torrents
 	tries   => 5,
-	creates => "/opt/KenyaEMR/downloads/${ovfImageName}",
-	}
+	creates => "/opt/KenyaEMR/${ovfImageName}",
+     }
 
 # a trick is used here: ;echo "" is used to run a successful command, expecting the delete to fail
-exec { "disableOldVM":
-	command => "vboxmanage unregistervm \"${oldVMName2Disable}\" --delete || echo \"\"",
-	}
 
-exec { "disableCurrentVM":
-	command => "vboxmanage unregistervm \"${oldVMName2Disable}\" --delete || echo \"\"",
+exec { "disableoldVM":
+	command => "/usr/bin/vboxmanage unregistervm \"${oldVMName2Disable}\" --delete || echo \"\"",
      }
+
+#exec { "disableCurrentVM":
+#	command => "/usr/bin/vboxmanage unregistervm \"${oldVMName2Disable}\" --delete || echo \"\"",
+#     }
 
 exec { "vboxImport":
-	command => "vboxmanage import /opt/KenyaEMR/downloads/${ovfImageName} --vsys 0 --vmname KEMR",
-	}
-exec { "startVM":
-	command => "vboxmanage start KEMR || echo vbmstart KEMR",
+	command => "/usr/bin/vboxmanage import /opt/KenyaEMR/${ovfImageName} --vsys 0 --vmname ${virtualMachineName}",
      }
-}
+
+exec { "startVM":
+	command => "/usr/bin/vboxmanage startvm ${virtualMachineName}",
+     }
